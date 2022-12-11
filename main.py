ad8c22c83c1d1e4644c64cf7c0db9f1b7869761a
rogarui <70268591+rogarui@users.noreply.github.com>
2022-12-11 11:17:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,28 +6,39 @@ import time
 import random
 from instaloader import Post
 
+
+#Set user, password and shortcode
+#Shortcode is like this https://www.instagram.com/p/Cl3nnL2oVrT/
+
+#Yo only neeed tu put the final part --> Cl3nnL2oVrT
+
+
 user = "XXXXXXX"
 password = "XXXXXXXX"
 media_id = Post.shortcode_to_mediaid("XXXXXXX")
 print(media_id)
 
+
+#Function to send messages based on the number of mentions, media_id, user and password
 def auto_sorteo(menciones, media_id, user, password):
+    
+    #log into instagram, may be the first time you need to battle with a instagram challange
     api = InstagramAPI.InstagramAPI(user, password)
     api.login()
-
+    
+    #Get the followers of your user
     followers = api.getTotalFollowers(api.username_id)
     photo_id = media_id
-
+    
+    #Set a vanilla counter and list to apply the number of mentions
     count = 0
     lista = []
     try:
         for follower in followers:
-
+            #Send message if the number of followers in the list is the same as the initial mentions
             if count == menciones:
                 #print("Mandando comentario al sorteo")
                 #print(lista)
-
-
                 comment_text =""
                 for x in range (0, len(lista)):
                     comment_text += "@" + lista[x] + " "
@@ -38,10 +49,11 @@ def auto_sorteo(menciones, media_id, user, password):
                 count = 1
                 lista = []
                 lista.append(follower)
+            #If not, add the follower to the list
             else:
                 lista.append(follower["username"])
                 count += 1
-
+            #Send the final list of followers
             if followers.index(follower) == (len(followers)-1):
                 comment_text =""
                 for x in range (0, len(lista)):
